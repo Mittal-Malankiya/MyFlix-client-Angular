60ffe2beeb9fd3fd644a6e576167b94cda3a9165
Mittal-Malankiya <m4mittal19@gmail.com>
2024-04-27 13:03:40 +0200

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'MyFlix-Angular-Client';
+  constructor(public dialog: MatDialog) { }
+  openUserRegistrationDialog(): void {
+    console.log('UserRegistrationFormComponent');
+    this.dialog.open(UserRegistrationFormComponent, { width: '280px' });
+  }
+}
