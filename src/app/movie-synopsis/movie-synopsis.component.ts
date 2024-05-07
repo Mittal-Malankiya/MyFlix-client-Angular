d6ae7e5b830dd48d79d87c7f7b451d05aac20f1f
Mittal-Malankiya <m4mittal19@gmail.com>
2024-05-07 08:09:19 +0200

    add typedoc comments

--- src/app/movie-synopsis/movie-synopsis.component.ts
+++ src/app/movie-synopsis/movie-synopsis.component.ts
@@ -1,12 +1,23 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+/**
+ * Component for displaying movie synopsis in a dialog.
+ */
+
 @Component({
   selector: 'app-movie-synopsis',
   templateUrl: './movie-synopsis.component.html',
   styleUrl: './movie-synopsis.component.scss'
 })
 export class MovieSynopsisComponent implements OnInit {
+
+  /**
+  * Constructor for MovieSynopsisComponent.
+  * @param dialogRef - Reference to the dialog opened by the component.
+  * @param data - Data passed to the dialog, including movieName and description.
+  */
+
   constructor(
     public dialogRef: MatDialogRef<MovieSynopsisComponent>,
     @Inject(MAT_DIALOG_DATA)
@@ -16,6 +27,9 @@ export class MovieSynopsisComponent implements OnInit {
     }
   ) { }
 
+  /**
+     * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+     */
   ngOnInit(): void {
   }
 }
